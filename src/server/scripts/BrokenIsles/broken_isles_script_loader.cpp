d6833a3d8c2c6405f85d9929877248cf92b1d735
Antonio M. Berti <15972392+BertiRean@users.noreply.github.com>
2024-02-01 09:24:16 -0300

    Scripts/BrokenIsles: Removed unused scripts

--- src/server/scripts/BrokenIsles/broken_isles_script_loader.cpp
+++ src/server/scripts/BrokenIsles/broken_isles_script_loader.cpp
@@ -16,11 +16,9 @@
  */
 
 // This is where scripts' loading functions should be declared:
-void AddSC_orderhall_warrior();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddBrokenIslesScripts()
 {
-    AddSC_orderhall_warrior();
 }
